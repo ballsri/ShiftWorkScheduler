d839c2bf0a012ff6987aaa7e25553c2b7c9de465
ballsri <ball04407@hotmail.com>
2023-03-11 05:07:59 +0700

    fully function schedule maker

---
+++ genSchedule.py
@@ -0,0 +1,140 @@
+import pandas as pd
+import xlsxwriter
+import datetime, calendar
+import numpy as np
+
+def genSchedule(df, month,monthStr, year):
+
+    workbook = xlsxwriter.Workbook('ตารางเวร' + monthStr + str(year+543) + '.xlsx')
+    worksheet = workbook.add_worksheet()
+
+
+    # variables
+    holiday = pd.Series(df['วันที่หยุด'].values, index=df['วันหยุด'].values).to_dict()
+    holiday = {k: int(v) for k, v in holiday.items() if str(k) != 'nan'}   
+    drivers = df['คนขับ'].tolist()
+    drivers = [x for x in drivers if str(x) != 'nan']
+    replace_driver = df['คนขับทดแทน'].tolist()[0]
+    dayShifters =list(df[['หัวหน้าเวรกลางวัน', 'ลูกเวรกลางวัน']].itertuples(index=False, name=None))
+    nightShifters = df['เวรกลางคืน'].tolist()
+    nightShifters = [x for x in nightShifters if str(x) != 'nan']
+
+    # day of week
+    numDay = calendar.monthrange(year, month)[1]
+    days = [datetime.date(year, month, day) for day in range(1, numDay+1)]
+    dayOfWeek = [day.strftime('%A') for day in days]
+    dayToThai = {'Monday': 'จันทร์', 'Tuesday': 'อังคาร', 'Wednesday': 'พุธ', 'Thursday': 'พฤหัสบดี', 'Friday': 'ศุกร์', 'Saturday': 'เสาร์', 'Sunday': 'อาทิตย์'}
+    dayOfWeek = [dayToThai[day] for day in dayOfWeek]
+    days = [day.strftime('%d') for day in days]
+
+    for k, v in holiday.items():
+        dayOfWeek[v-1] = dayOfWeek[v-1] + ' (' + k + ')'
+
+    # day of holiday
+    holidayIndex = [i for i, day in enumerate(dayOfWeek) if '(' in day or day == 'อาทิตย์' or day == 'เสาร์']
+
+    # shuffle shifters
+    np.random.shuffle(drivers)
+    np.random.shuffle(dayShifters)
+    np.random.shuffle(nightShifters)
+
+    # create schedule
+    dayShift = [""] * numDay
+    nightShift = [""] * numDay
+    holidayDriverShift = [""] * numDay
+    normDriverShift = [""] * numDay
+    bookShift = [""] * numDay
+
+    # assign day shift
+    j = 0
+    for i in holidayIndex:
+        dayShift[i] = dayShifters[j]
+        j = (j+1) % len(dayShifters)
+
+    # assign night shift
+    j = 0
+    for i in range(numDay):
+        nightShift[i] = nightShifters[j]
+        j = (j+1) % len(nightShifters)
+
+    # assign holiday driver shift
+    j = 0
+    for i in holidayIndex:
+        holidayDriverShift[i] = drivers[j]
+        j = (j+1) % len(drivers)
+
+    # assign normal driver shift
+    # remove replace driver from drivers
+    drivers.remove(replace_driver)
+    
+    j = 0
+    for i in range(numDay):
+        if dayShift[i] == "":
+            normDriverShift[i] = drivers[j]
+            j = (j+1) % len(drivers)
+
+    
+    # swap one of the normak driver with replace driver to do another task
+    # one job per week
+    # 3 -> 11 -> 19 -> 27 -> 30 ( start new week)
+    # find the first non holiday day
+    for i in range(numDay):
+        if dayShift[i] == "":
+            break
+    # swap driver in the next 7 days and iterate it
+    while i <= numDay:
+        bookShift[i] = normDriverShift[i]
+        normDriverShift[i] = replace_driver
+        if dayOfWeek[i] == "ศุกร์":
+            i = (i + 3)
+        else:
+            i = (i + 8)
+
+    # create output dataframe
+    output = pd.DataFrame({'วันที่': days, 'วัน': dayOfWeek,'ส่งหนังสือ': bookShift, 'เวรขับรถเย็น': normDriverShift, 'เวรขับรถวันหยุด': holidayDriverShift, 'เวรกลางวัน': dayShift, 'เวรกลางคืน': nightShift})
+
+    # save to excel
+    header_format = workbook.add_format({'bold': True, 'font_size': 18, 'align': 'center', 'valign': 'vcenter', 'bg_color': '#D9D9D9'})
+    subcell_format = workbook.add_format({'border': 1, 'align': 'center', 'valign': 'vcenter'})
+    holiday_format = workbook.add_format({'border': 1, 'align': 'center', 'valign': 'vcenter', 'bg_color': '#ffc34d'})
+    weekend_format = workbook.add_format({'border': 1, 'align': 'center', 'valign': 'vcenter', 'bg_color': '#88cc00'})
+
+    row = 0
+    col = 0
+    # write title
+    worksheet.merge_range(row,col,row,col+7, "ตารางเวร ประจำเดือน " + monthStr + " ปี พ.ศ."+ str(year+543), header_format)
+    row += 1
+    # write header
+    worksheet.write(row,col, "วันที่", subcell_format)
+    worksheet.write(row,col+1, "วัน", subcell_format)
+    worksheet.write(row,col+2, "ส่งหนังสือ", subcell_format)
+    worksheet.write(row,col+3, "เวรขับรถเย็น", subcell_format)
+    worksheet.write(row,col+4, "เวรขับรถวันหยุด", subcell_format)
+    worksheet.merge_range(row,col+5,row,col+6, "เวรกลางวัน", subcell_format)
+    worksheet.write(row,col+7, "เวรกลางคืน", subcell_format)
+    row += 1
+
+    for index, r in output.iterrows():
+        if r['วัน'] == 'อาทิตย์' or r['วัน'] == 'เสาร์':
+            cell_format = weekend_format
+        elif '(' in r['วัน']:
+            cell_format = holiday_format
+        else:
+            cell_format = subcell_format
+        worksheet.write(row,col, r['วันที่'], cell_format)
+        worksheet.write(row,col+1, r['วัน'], cell_format)
+        worksheet.write(row,col+2, r['ส่งหนังสือ'], cell_format)
+        worksheet.write(row,col+3, r['เวรขับรถเย็น'], cell_format)
+        worksheet.write(row,col+4, r['เวรขับรถวันหยุด'], cell_format)
+        if r['เวรกลางวัน'] != "":
+            worksheet.write(row,col+5, r['เวรกลางวัน'][0], cell_format)
+            worksheet.write(row,col+6, r['เวรกลางวัน'][1], cell_format)
+        else:
+            worksheet.write(row,col+5, "", cell_format)
+            worksheet.write(row,col+6, "", cell_format)
+        worksheet.write(row,col+7, r['เวรกลางคืน'], cell_format)
+        row += 1
+
+    workbook.close()
+    
+    
---
+++ main.py
@@ -0,0 +1,141 @@
+from tkinter import *
+from tkinter import filedialog, messagebox
+from PIL import ImageTk, Image
+import os
+import pandas as pd
+import datetime
+from genSchedule import genSchedule as gs
+
+def rcpath(rel_path):
+    return os.path.join(os.getcwd(), rel_path)
+
+root = Tk()
+root.title("ShiftWorkScheduler")
+# root.geometry("640x480")
+root.resizable(False, False)
+root.iconphoto(True, ImageTk.PhotoImage(Image.open(rcpath("icon.jpg"))))
+
+# Global variables
+root.filename = ""
+df = pd.DataFrame()
+month = 0
+curr_year = datetime.datetime.now().year
+
+# Create a frame
+frame = Frame(root)
+frame.pack()
+
+# Create a label
+label_frame = Frame(frame)
+label_frame.grid(row = 0, column = 0, padx = 10, pady = 10)
+
+label = Label(label_frame, text = "โปรแกรมจัดตารางเวรจากไฟล์ Excel", font = ("Arial", 20))
+label.grid(row = 0, column = 0, padx = 10, pady = 10)
+
+year_label = Label(label_frame, text = "ปี : " + str(curr_year), font = ("Arial", 20))
+year_label.grid(row = 0, column = 1, padx = 10, pady = 10)
+
+# Create an section
+input_frame = Frame(frame)
+input_frame.grid(row = 1, column = 0, padx = 10, pady = 10)
+
+lFrame = Frame(input_frame)
+lFrame.grid(row = 1, column = 0, padx = 10, pady = 10)
+rFrame = Frame(input_frame)
+rFrame.grid(row = 1, column = 1, padx = 10, pady = 10)
+
+# Create an input button
+def openExcel():
+    global df
+    try:
+        root.filename = filedialog.askopenfilename( title = "Select an Excel File", filetypes = (("Excel Worksheet", "*.xlsx"), ("Excel 97-2003 Worksheet", "*.xls")))
+        df = pd.read_excel(root.filename)
+        df.dropna(axis = 1)
+        # print(df)
+        input_btn.config(text = "นำเข้าข้อมูลเรียบร้อยแล้ว", state= DISABLED)
+    except:
+        messagebox.showinfo("Error", "โปรดเลือกไฟล์ Excel หรือ ปิด Excel ก่อนนำเข้าข้อมูล")
+    
+
+
+input_label = Label(lFrame, text = "กดปุ่มเพื่อนำเข้าข้อมูลจาก Excel", font = ("Arial", 20))
+input_label.grid(row = 0, column = 0, padx = 10, pady = 10)
+
+input_btn = Button(lFrame, text = "เลือกไฟล์ข้อมูล Excel", font= ('Arial', 20), command = openExcel, bd=5)
+input_btn.grid(row = 1, column = 0, padx = 10, pady = 10)
+
+def resetData():
+    # graphical reset
+    input_btn.config(text = "เลือกไฟล์ข้อมูล Excel", state = NORMAL)
+    input_month_label.config(text = "โปรดเลือกเดือน")
+   
+    listbox.selection_clear(0, END)
+    listbox.activate(0)
+    listbox.selection_set(0)
+    listbox.selection_anchor(0)
+
+     # data reset
+    global df
+    global month
+    root.filename = ""
+    df = pd.DataFrame()
+    month = 0
+
+
+reset_btn = Button(lFrame, text = "รีเซ็ตข้อมูล", font = ('Arial', 20), bd = 5, command = resetData)
+reset_btn.grid(row = 2, column = 0, padx = 10, pady = 10)
+
+# Create month select frame
+month_frame = Frame(rFrame)
+month_frame.grid(row = 1, column = 0, padx = 10, pady = 10)
+
+# Create a lable
+month_text = "โปรดเลือกเดือน"
+input_month_label = Label(month_frame, text = month_text, font = ("Arial", 20))
+input_month_label.grid(row = 0, column = 0, padx = 10, pady = 10)
+
+# Create submit month button
+
+def selectMonth():
+    global month
+
+    if listbox.get(ANCHOR) == "":
+        messagebox.showinfo("Error", "โปรดเลือกเดือน")
+    else:
+        input_month_label.config(text = "เดือน : " + listbox.get(ANCHOR))
+        month = months.index(listbox.get(ANCHOR)) + 1
+        
+
+submit_month_btn = Button(month_frame, text = "ยืนยันเดือน", font = ('Arial', 20), bd= 5, command= selectMonth)
+submit_month_btn.grid(row = 0, column = 1, padx = 10, pady = 10)
+
+# Create listbox
+months = ["มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "มิถุนายน", "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"]
+listbox = Listbox(rFrame, width = 20, height = 10, font = ('Arial', 20) )
+listbox.grid(row = 0, column = 0, padx = 10, pady = 10)
+for m in months:
+    listbox.insert(END, m)
+listbox.activate(0)
+listbox.selection_set(0)
+listbox.selection_anchor(0)
+
+# Create a generate button
+def genSchedule():
+    if root.filename == "":
+        messagebox.showinfo("Error", "โปรดนำเข้าข้อมูลจาก Excel")
+    elif month == 0:
+        messagebox.showinfo("Error", "โปรดเลือกเดือน")
+    else:
+        gs(df, month,months[month-1], curr_year)
+        messagebox.showinfo("Success", "สร้างตารางเวรเรียบร้อยแล้ว")
+
+submit_label = Label(lFrame, text = "กดปุ่มเพื่อสร้างตารางเวร", font = ("Arial", 20))
+submit_label.grid(row = 3, column = 0, padx = 10, pady = 10)
+
+gen_btn = Button(lFrame, text = "สร้างตารางเวร", font = ('Arial', 20), bd = 5, command = genSchedule)
+gen_btn.grid(row = 4, column = 0, padx = 10, pady = 10)
+
+
+
+root.mainloop()
+
